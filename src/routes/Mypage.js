63ac047c8d61d1bd7d7f9cf345d5b00723d30561
harim061 <doris5093@duksung.ac.kr>
2023-04-14 22:37:22 +0900

    g

--- src/routes/Mypage.js
+++ src/routes/Mypage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-// import MindsLists from '../components/PostView/MindsList';
+
 import Mypages from './../components/Mypage/Mypage.js';
 
 export default function Mypage({ setLoginStateFalse }) {
